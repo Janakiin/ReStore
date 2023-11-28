2651bdb7befd366c6ac77f07da82dc4261eb5845
Janakiin <59832231+Janakiin@users.noreply.github.com>
2023-11-28 17:45:38 +0100

    end of Section 5

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,43 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionMiddleware> logger;
+        private readonly IHostEnvironment env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            this.next = next;
+            this.logger = logger;
+            this.env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try{
+                await next(context);
+            }
+            catch (Exception exception){
+                logger.LogError(exception, exception.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = 500;
+
+                var response = new ProblemDetails
+                {
+                    Status = 500,
+                    Detail = env.IsDevelopment() ? exception.StackTrace?.ToString() : null,
+                    Title = exception.Message
+                };
+
+                var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
